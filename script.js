6086a24a875852194d795fc6c2ba4cf5cd816139
LawrieDrew <laurenn.drew@gmail.com>
2021-02-20 19:37:58 -0500

    added check box form code to HTML in order to grab the check boxes in the password generator javascript code

--- script.js
+++ script.js
@@ -7,43 +7,52 @@
 var generateBtn = document.querySelector("#generate");
 
 // Set up string references for password criteria to prepare app for all (permissible) possible combinations 
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var symbols = "!@#$%^&*~";
-
+var allowed = "";
+var password = "";
 // Add event listener to generate button, placed after strings to follow chronological logic
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 //Now we tell our javascript what to do with the above information
-
-var generatePassword = function() {
- //We have to set password to nothing at first to start from scratch. Password is empty space we can add a value to
-var password = []
-}
 //length prompt appears asking user to select a password character length
-var passwordLength = [];
 //if user chooses a charcter length between 8 and 128 they can continue
-if (passwordLength => 8 && passwordLength =< 126); {
-
-    console.log(passwordLength);
-}
 //if user does not choose a character length between 8 and 128 they are prompted to try again
-
-"Please select a character length between 8 and 126 and try again!"
-//number checkboxes: if user wants to use a number in their password they can elect to do so
-//special character checkboxes: if user wants to utilize special characters in their password they can elect to do so
-//run checks to make sure all criteria are correct lest the action bounce back to square one
-=== statements to confirm  all 
-//randomizer takes password "recipe" and mixes it
-generate random code
-//spits password back out to user!
-
+function writePassword() {
+  var password = generatedPassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+  var passwordLength = window.prompt("Enter a number between 8 - 128 characters:");
+  if (!passwordLength) {
+      window.alert("Please choose a number between 8 - 128 characters. Please try again.");
+      return;
+  }
+  else if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("Please choose a number between 8 - 128 characters. Please try again.");
+      return;
+  }
+function generatedPassword (){
+    if (document.generator.upperCase.checked) {
+        allowed = allowed + upperCase;
+    }
+    if (document.generator.lowercase.checked) {
+        allowed = allowed + lowercase;
+    }
+    if (document.generator.numbers.checked) {
+        allowed = allowed + numbers;
+    }
+    if (document.generator.symbols.checked) {
+        allowed = allowed = symbols;
+    }
+};
+    var passwordLength = window.prompt("Enter a number between 8 - 128 characters:");
+    if (!passwordLength) {
+        window.alert("Please choose a number between 8 - 128 characters. Please try again.");
+        return;
+    }
+    else if (passwordLength < 8 || passwordLength > 128) {
+        window.alert("Please choose a number between 8 - 128 characters. Please try again.");
+        return};
